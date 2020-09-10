1d44c3251ddee20397a3c3415bf33487ce890b73
k0ded <liammaster10@hotmail.com>
2020-09-10 10:56:29 +0200

    COMMIT BITCH

---
+++ Lektion/TextUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lektion
+{
+    public static class TextUtils
+    {
+        private static int aiGuesses = 0;
+        private static int playerGuesses = 0;
+        
+        public static void SendPlayerMessage(string message)
+        {
+            var x = Console.WindowWidth / 4;
+            var y = 2 + playerGuesses;
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(message);
+            playerGuesses++;
+        }
+        
+        public static void SendAIMessage(string message)
+        {
+            var x = Console.WindowWidth / 4 * 3;
+            var y = 2 + aiGuesses;
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(message);
+            aiGuesses++;
+        }
+        
+    }
+}
